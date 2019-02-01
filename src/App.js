d1546562bb50fa7d82a2d0d70a2dabfea420e91f
albahniuk <albalopezfolgar@gmail.com>
2019-02-01 09:14:34 +0100

    finish basic structure

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header>
+      <div className="app-container">
+        <header className="header-container">
           <div className="header__logo">
             <img src="" alt=""></img>
           </div>
           <div className="header__link"><a>How it works</a></div>
         </header>
-        <main>
+        <main className="main-container">
           <div className="home-page">
           {/* header */}
-            <div>
-              <button>Sign in</button>
+            <div className="home-page__btn">
+              <button type="button">Sign in</button>
             </div>
           {/* footer */}
           </div>
@@ -26,11 +26,11 @@ class App extends Component {
               <div className="wizard__item step2">Fill the fields</div>
               <div className="wizard__item step3">Review and download</div>
             </div>
-            <div>
-              <button>Select template</button>
+            <div className="choose-page__btn select-btn">
+              <button type="button">Select template</button>
             </div>
-            <div>
-              <button>Upload template</button>
+            <div className="choose-page__btn upload-btn">
+              <button type="button">Upload template</button>
             </div>
             {/* footer */}
           </div>
@@ -39,14 +39,34 @@ class App extends Component {
             {/* wizard */}
             <div className="fill-page__form">
               <form>
-                <label></label>
-                <input type="text"></input>
+                <label for=""></label>
+                <input id="" type="text"></input>
               </form>
+            </div>
+            <div className="fill-page__btn back-btn">
+              <button type="button">Back</button>
+            </div>
+            <div className="fill-page__btn next-btn">
+              <button type="button">Next</button>
+            </div>
+            {/* footer */}
+          </div>
+          <div className="success-page">
+            {/* header */}
+            {/* wizard */}
+            <div className="success-page__icons">
+
+            </div>
+            <div className="success-page__btn view-btn">
+              <button>View your presentation</button>
+            </div>
+            <div className="success-page__btn download-btn">
+              <button>Download your presentation</button>
             </div>
             {/* footer */}
           </div>
         </main>
-        <footer>
+        <footer className="footer-container">
 
         </footer>
       </div>
