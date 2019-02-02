95fe290f69fee14c632070320f8c92145786f079
babelarr <babelarr@gmail.com>
2019-02-02 21:35:15 +0100

    resolved some problems with nav styles

--- src/components/Wizard.js
+++ src/components/Wizard.js
@@ -4,19 +4,21 @@ import {Link} from 'react-router-dom';
 class Wizard extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <ul className="nav">
-          <li className="nav-item">
-            <Link className="nav-link active" to="/steps/choose">Choose your template</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/steps/fill">Fill the fields</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/steps/success">Review and download</Link>
-          </li>
-        </ul>
-      </nav>
+      <div className="wizard">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="wizard-list nav">
+            <li className="wizard__item step1 nav-item">
+              <Link className="nav-link active" to="/steps/choose">Choose your template</Link>
+            </li>
+            <li className="wizard__item step2 nav-item">
+              <Link className="nav-link" to="/steps/fill">Fill the fields</Link>
+            </li>
+            <li className="wizard__item step3 nav-item">
+              <Link className="nav-link" to="/steps/success">Review and download</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     );
   }
 }
