a1bad40d7bb58e2cc73a90c7d1ffa3d8808b89d4
roxsb <rox.san.bri@gmail.com>
2019-02-05 12:23:48 +0100

    try to solve picker functionality

--- src/components/ApiPicker.js
+++ src/components/ApiPicker.js
@@ -9,53 +9,65 @@ class ApiPicker extends Component {
       pickerApiLoaded: false
     }
 
+    this.onApiLoad = this.onApiLoad.bind(this);
     this.onPickerApiLoad = this.onPickerApiLoad.bind(this);
     this.handleAuthResult = this.handleAuthResult.bind(this);
     this.createPicker = this.createPicker.bind(this);
     this.pickerCallback = this.pickerCallback.bind(this);
     this.onAuthApiLoad = this.onAuthApiLoad.bind(this);
+
   }
 
   componentDidMount(){
+    console.log('me monté');
     const script = document.createElement('script');
     script.type = 'text/javascript';
     script.async = true;
     script.defer = true;
-    script.src = 'https://apis.google.com/js/api.js';
+    script.src = 'https://apis.google.com/js/api.js?onload=onApiLoad';
 
     const tag = document.getElementsByTagName('script')[0];
     tag.parentNode.insertBefore(script, tag);
+    this.onApiLoad();
   }
 
   onApiLoad() {
+    console.log('onApiLoad');
     window.gapi.load('auth2', this.onAuthApiLoad);
     window.gapi.load('picker', this.onPickerApiLoad);
   }
 
   onPickerApiLoad() {
+    console.log('onPickerApiLoad');
     this.setState({
       pickerApiLoaded: true
     })
-    this.createPicker();
+    // this.createPicker();
   }
 
   onAuthApiLoad(){
+    console.log('autorizado');
     window.gapi.auth2.authorize({
       client_id: this.props.clientId,
       scope: this.props.scopes
-    }, this.handleAuthResult());
+    }, this.handleAuthResult);
   }
 
   handleAuthResult(authResult) {
+    console.log('clicaste');
     if (authResult && !authResult.error) {
       oauthToken = authResult.access_token;
+      console.log(oauthToken);
       this.createPicker();
     }
   }
 
   createPicker() {
+    console.log('1');
+    console.log(oauthToken)
     let { pickerApiLoaded } = this.state;
     if (pickerApiLoaded && oauthToken) {
+      console.log('if funciona')
       let picker = new window.google.picker.PickerBuilder()
         .addView(window.google.picker.ViewId.PRESENTATIONS)
         .setOAuthToken(oauthToken)
@@ -67,6 +79,7 @@ class ApiPicker extends Component {
   }
 
   pickerCallback(data) {
+    console.log('2');
     let url = 'nothing';
     if (data[window.google.picker.Response.ACTION] === window.google.picker.Action.PICKED) {
       let doc = data[window.google.picker.Response.DOCUMENTS][0];
@@ -78,7 +91,7 @@ class ApiPicker extends Component {
 
   render() {
     return (
-      <button type="button" onClick={this.onAuthApiLoad}>Select</button>
+      <button type="button" onClick={this.handleAuthResult}>Select</button>
     );
   }
 }
--- src/components/Choose.js
+++ src/components/Choose.js
@@ -12,7 +12,33 @@ class Choose extends Component {
         <div className="choose-page__btn upload-btn">
           <button type="button" className="btn btn-secondary btn-lg">Upload template</button>
         </div>
-        <ApiPicker clientId={clientId} discoveryDocs={discoveryDocs} scopes={scopes} apiKey={apiKey} signIn={signIn}/>
+        <ApiPicker clientId={clientId} discoveryDocs={discoveryDocs} scopes={scopes} apiKey={apiKey} signIn={signIn}
+
+        developerKey={apiKey}
+        onChange={data => console.log('on change:', data)}
+        onAuthFailed={data => console.log('on auth failed:', data)}
+        multiselect={true}
+        navHidden={true}
+        authImmediate={false}
+        viewId={'FOLDERS'}
+        createPicker={ (google, oauthToken) => {
+          const googleViewId = google.picker.ViewId.FOLDERS;
+          const docsView = new google.picker.DocsView(googleViewId)
+              .setIncludeFolders(true)
+              .setMimeTypes('application/vnd.google-apps.folder')
+              .setSelectFolderEnabled(true);
+
+          const picker = new window.google.picker.PickerBuilder()
+              .addView(docsView)
+              .setOAuthToken(oauthToken)
+              .setDeveloperKey(apiKey)
+              .setCallback(()=>{
+                console.log('Custom picker is ready!');
+              });
+
+          picker.build().setVisible(true);
+      }}
+              />
       </div>
     );
   }
