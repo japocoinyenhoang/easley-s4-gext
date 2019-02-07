db5cca3944406e84a551c6c8c72b7e86a3177ecb
JapocoinYenHoang <japocoin@gmail.com>
2019-02-07 09:05:01 +0100

    create build and change it to docs

---
+++ docs/precache-manifest.0be93a5ef00e57016b2022a21925d1c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cdc8fce9609d07f0e9d8d0bc4b61f8f",
+    "url": "./static/media/btn_google.1cdc8fce.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "2c9880f5bd753edc0a42",
+    "url": "./static/js/main.2c9880f5.chunk.js"
+  },
+  {
+    "revision": "4a0f465a2595a09b78c1",
+    "url": "./static/js/1.4a0f465a.chunk.js"
+  },
+  {
+    "revision": "2c9880f5bd753edc0a42",
+    "url": "./static/css/main.ab6aba05.chunk.css"
+  },
+  {
+    "revision": "7be6f1aa54aa8c55d8498169d997c759",
+    "url": "./index.html"
+  }
+];
