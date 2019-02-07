441d329fe7619b14952a7f42b7f2d29b242ab121
Silvia García Cacho <silvia@garca.info>
2019-02-07 19:26:12 +0100

    Clear code

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 import HeaderHome from './HeaderHome';
 import logo from '../images/logo__gext.png';
 import ApiLogin from './ApiLogin';
