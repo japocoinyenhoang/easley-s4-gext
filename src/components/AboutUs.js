8473415edbe66bc1ac60f234c06c632fb4df4507
roxsb <rox.san.bri@gmail.com>
2019-02-16 18:36:21 +0100

    creating material style on aboutUs

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -5,84 +5,140 @@ import alba from '../images/alba.jpg';
 import yen from '../images/yen.jpg';
 import roxana from '../images/roxana.jpg';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Radio from '@material-ui/core/Radio';
+import Paper from '@material-ui/core/Paper';
+import Avatar from '@material-ui/core/Avatar';
+
+const styles = theme => ({
+  avatar: {
+    margin: "10px",
+  },
+  bigAvatar: {
+    margin: "10px",
+    width: "60px",
+    height: "60px",
+  },
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: "140px",
+    width: "100px",
+  },
+  control: {
+    padding: "200px",
+  },
+});
 
 class AboutUs extends Component {
-  render() {
+  state = {
+    direction: 'column',
+    justify: 'center',
+    alignItems: 'strech',
+  };
+
+   render() {
+    const {classes} = this.props
+
     return (
-      <div className='section__about-us'>
-        <ul className='about-us__list'>
-          <li className='about-us__item'>
-            <div className='about-us__pic'>
-              <img className='profile-pic' src={silvia} alt='profile pic'></img>
-            </div>
-            <h2 className='about-us__title'>Silvia García Cacho</h2>
-            <p className='about-us__description'>Master in cultural management. Due to my interest in technology and continuous learning I decided to start a new adventure as front-end developer. Hope to keep helping cultural institutions to embrace the XXI Century technology.</p>
-            <ul className='social-media__list'>
-              <li className='social-media__item'>Twitter: https://twitter.com/garcaplay</li>
-              <li className='social-media__item'>Github: https://github.com/garcaplay</li>
-              <li className='social-media__item'>Linkedin: https://www.linkedin.com/in/silvia-garcía-cacho/</li>
-              <li className='social-media__item'>Correo: silvia@garca.info</li>
-            </ul>
-          </li>
-          <li className='about-us__item'>
-            <div className='about-us__pic'>
-              <img className='profile-pic' src={alba} alt='profile pic'></img>
-            </div>
-            <h2 className='about-us__title'>Alba López Folgar</h2>
-            <p className='about-us__description'>Graduated in Sociology, driven by social questions and issues. After studying in Adalab and develop the necessary skills to start a new path on web development, I am very eager to develop my career and never stop learning and growing.</p>
-            <ul className='social-media__list'>
-              <li className='social-media__item'>Twitter: https://twitter.com/albahniuk</li>
-              <li className='social-media__item'>Github: https://github.com/albahniuk</li>
-              <li className='social-media__item'>Linkedin: https://www.linkedin.com/in/albalopezfolgar/</li>
-              <li className='social-media__item'>Correo: albalopezfolgar@gmail.com</li>
-            </ul>
-          </li>
-          <li className='about-us__item'>
-            <div className='about-us__pic'>
-              <img className='profile-pic' src={roxana} alt='profile pic'></img>
-            </div>
-            <h2 className='about-us__title'>Roxana Sánchez Briñas</h2>
-            <p className='about-us__description'>After graduating on Fine Arts, I implemented several multidisciplinary projects while serving coffees during the day and photographing "zombies" during the night on dive bars. Nowadays, I have jumped into the digital world, where I hope to keep evolving and acomplish in life, as her "Sim" does.</p>
-            <ul className='social-media__list'>
-              <li className='social-media__item'>Twitter: https://twitter.com/RoxSBri</li>
-              <li className='social-media__item'>Github: https://github.com/roxsb</li>
-              <li className='social-media__item'>Linkedin: https://www.linkedin.com/in/roxana-sb/</li>
-              <li className='social-media__item'>Correo: rox.san.bri@gmail.com</li>
-            </ul>
-          </li>
-          <li className='about-us__item'>
-            <div className='about-us__pic'>
-              <img className='profile-pic' src={yen} alt='profile pic'></img>
-            </div>
-            <h2 className='about-us__title'>Yen Hoang</h2>
-            <p className='about-us__description'>Future front-end developer, passionate about flex-box, currently trying to keep me afloat in the world of code. Philosophy of life: Work hard and win.</p>
-            <ul className='social-media__list'>
-              <li className='social-media__item'>Twitter: https://twitter.com/JapocoinYH</li>
-              <li className='social-media__item'>Github: https://github.com/japocoinyenhoang</li>
-              <li className='social-media__item'>Linkedin: https://www.linkedin.com/in/YenHoangChu/</li>
-              <li className='social-media__item'>Correo: japocoin@gmail.com</li>
-            </ul>
-          </li>
-          <li className='about-us__item'>
-            <div className='about-us__pic'>
-              <img className='profile-pic' src={laura} alt='profile pic'></img>
-            </div>
-            <h2 className='about-us__title'>Laura Sánchez Redondo</h2>
-            <p className='about-us__description'>I changed my career as a Teacher of Infant Education and English, for UX Design. I discovered Adalab through a group of Spanish Ux Ladies on Twitter and, at the first time, I thought it was the best option to develop my professional career in the technological world. I love being part of this great initiative.</p>
-            <ul className='social-media__list'>
-              <li className='social-media__item'>Twitter: https://twitter.com/babelarr</li>
-              <li className='social-media__item'>Github: https://github.com/babelarr</li>
-              <li className='social-media__item'>Linkedin: https://www.linkedin.com/in/laurasanchezredondo/</li>
-              <li className='social-media__item'>Correo: babelarr@gmail.com</li>
-            </ul>
-          </li>
-        </ul>
-        <div>
-          <Link className="link__about-us" to="/">Go back</Link>
+      <Grid container justify="center" alignItems="center" className={classes.root} spacing={16}>
+
+        <Grid item xs={12}>
+          <Grid container className={classes.demo} justify="center" spacing={Number(this.spacing)}>
+            <Paper className={classes.paper} />
+          <Grid item xs={12}></Grid>
+          </Grid>
+        </Grid>
+        <div className='section__about-us'>
+          <ul className='about-us__list'>
+            <li className='about-us__item'>
+              <div className='about-us__pic'>
+                <img className='profile-pic' src={silvia} alt='profile pic'></img>
+              </div>
+              <h2 className='about-us__title'>Silvia García Cacho</h2>
+              <p className='about-us__description'>Master in cultural management. Due to my interest in technology and continuous learning I decided to start a new adventure as front-end developer. Hope to keep helping cultural institutions to embrace the XXI Century technology.</p>
+              <ul className='social-media__list'>
+                <li className='social-media__item'>Twitter: https://twitter.com/garcaplay</li>
+                <li className='social-media__item'>Github: https://github.com/garcaplay</li>
+                <li className='social-media__item'>Linkedin: https://www.linkedin.com/in/silvia-garcía-cacho/</li>
+                <li className='social-media__item'>Correo: silvia@garca.info</li>
+              </ul>
+            </li>
+            <li className='about-us__item'>
+              <div className='about-us__pic'>
+              <Grid container justify="center" alignItems="center">
+                <Avatar alt="Remy Sharp" src={alba} className={classes.bigAvatar} />
+              </Grid>
+                <img className='profile-pic' src={alba} alt='profile pic'> </img>
+              </div>
+              <h2 className='about-us__title'>Alba López Folgar</h2>
+              <p className='about-us__description'>Graduated in Sociology, driven by social questions and issues. After studying in Adalab and develop the necessary skills to start a new path on web development, I am very eager to develop my career and never stop learning and growing.</p>
+              <ul className='social-media__list'>
+                <li className='social-media__item'>Twitter: https://twitter.com/albahniuk</li>
+                <li className='social-media__item'>Github: https://github.com/albahniuk</li>
+                <li className='social-media__item'>Linkedin: https://www.linkedin.com/in/albalopezfolgar/</li>
+                <li className='social-media__item'>Correo: albalopezfolgar@gmail.com</li>
+              </ul>
+            </li>
+            <li className='about-us__item'>
+              <div className='about-us__pic'>
+                <img className='profile-pic' src={roxana} alt='profile pic'></img>
+              </div>
+              <h2 className='about-us__title'>Roxana Sánchez Briñas</h2>
+              <p className='about-us__description'>After graduating on Fine Arts, I implemented several multidisciplinary projects while serving coffees during the day and photographing "zombies" during the night on dive bars. Nowadays, I have jumped into the digital world, where I hope to keep evolving and acomplish in life, as her "Sim" does.</p>
+              <ul className='social-media__list'>
+                <li className='social-media__item'>Twitter: https://twitter.com/RoxSBri</li>
+                <li className='social-media__item'>Github: https://github.com/roxsb</li>
+                <li className='social-media__item'>Linkedin: https://www.linkedin.com/in/roxana-sb/</li>
+                <li className='social-media__item'>Correo: rox.san.bri@gmail.com</li>
+              </ul>
+            </li>
+            <li className='about-us__item'>
+              <div className='about-us__pic'>
+                <img className='profile-pic' src={yen} alt='profile pic'></img>
+              </div>
+              <h2 className='about-us__title'>Yen Hoang</h2>
+              <p className='about-us__description'>Future front-end developer, passionate about flex-box, currently trying to keep me afloat in the world of code. Philosophy of life: Work hard and win.</p>
+              <ul className='social-media__list'>
+                <li className='social-media__item'>Twitter: https://twitter.com/JapocoinYH</li>
+                <li className='social-media__item'>Github: https://github.com/japocoinyenhoang</li>
+                <li className='social-media__item'>Linkedin: https://www.linkedin.com/in/YenHoangChu/</li>
+                <li className='social-media__item'>Correo: japocoin@gmail.com</li>
+              </ul>
+            </li>
+            <li className='about-us__item'>
+              <div className='about-us__pic'>
+                <img className='profile-pic' src={laura} alt='profile pic'></img>
+              </div>
+              <h2 className='about-us__title'>Laura Sánchez Redondo</h2>
+              <p className='about-us__description'>I changed my career as a Teacher of Infant Education and English, for UX Design. I discovered Adalab through a group of Spanish Ux Ladies on Twitter and, at the first time, I thought it was the best option to develop my professional career in the technological world. I love being part of this great initiative.</p>
+              <ul className='social-media__list'>
+                <li className='social-media__item'>Twitter: https://twitter.com/babelarr</li>
+                <li className='social-media__item'>Github: https://github.com/babelarr</li>
+                <li className='social-media__item'>Linkedin: https://www.linkedin.com/in/laurasanchezredondo/</li>
+                <li className='social-media__item'>Correo: babelarr@gmail.com</li>
+              </ul>
+            </li>
+          </ul>
+          <div>
+            <Link className="link__about-us" to="/">Go back</Link>
+          </div>
         </div>
-      </div>
+      </Grid>
+
     );
   }
 }
 
-export default AboutUs;
+AboutUs.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles (AboutUs)(styles);
