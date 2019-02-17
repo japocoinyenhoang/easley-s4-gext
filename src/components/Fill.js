bc95ac48e06a2f51d2dbbfe49c4c17b4df1386f3
babelarr <babelarr@gmail.com>
2019-02-17 11:10:38 +0100

    add material ui styles in fill page

--- src/components/Fill.js
+++ src/components/Fill.js
@@ -3,6 +3,53 @@ import { Link } from 'react-router-dom';
 import PropTypes from "prop-types";
 import ReactLoading from 'react-loading';
 
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Fab from '@material-ui/core/Fab';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  message: {
+    padding: `${theme.spacing.unit * 2}px`,
+    margin:  `0 ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+    backgroundColor: "#2196f3",
+    color: theme.palette.primary.contrastText,
+  },
+  widthStyle: {
+    margin:  `0 ${theme.spacing.unit * 3}px`,
+  },
+  paperFill: {
+    color: theme.palette.primary.contrastText,
+  },
+  links: {
+    textDecoration:"none",
+    color: "unset"
+  },
+  btn:{
+    color: theme.palette.primary.contrastText,
+  },
+  form: {
+    padding: `${theme.spacing.unit * 2}px`,
+    margin:  `0 ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+  },
+  marginIcon: {
+    marginRight: theme.spacing.unit,
+  },
+  btnSend: {
+    margin: `${theme.spacing.unit * 2}px`,
+  },
+  itemBtn:{
+    textAlign: "center",
+  },
+
+});
+
 let keywords = [];
 let eraseMoustache;
 let presentation;
@@ -83,31 +130,86 @@ class Fill extends Component {
   }
 
   paintForm() {
-    const { handleInputs, handleChangeFile, fileInput, fakeClick } = this.props;
+    const { handleInputs, handleChangeFile, fileInput, fakeClick, classes } = this.props;
     if (this.state.moustachesArray.length === 0 && this.state.tripleMoustachesArray.length === 0) {
       return (<div className="errorMessage">Sorry but your template has not any keyword to create a form. Please review our 'How to use' section</div>)
     } else {
       return (
         <form>
-          {this.state.moustachesArray.map(item => {
-            return (
-              <div key={item} className="form-group">
-                <label htmlFor={item}>{item.toUpperCase()}:</label>
-                <input className="form-control " id={item} type="text" onKeyUp={handleInputs} />
-              </div>
-            );
-          })
-          }
-
-          {this.state.tripleMoustachesArray.map(item=>{
-            return (
-              <div key={item} className="form-group">
-                    <label htmlFor={item}>{item.toUpperCase()}:</label>
-                    <input className="form-control " id={item} type="file" ref={fileInput} onChange={handleChangeFile} />
-                  </div>
-            );
-          })
-          }
+          <Paper className={classes.form} elevation={1}>
+            <Typography variant="h5" component="h2">Fill the form</Typography>
+            <Grid container justify="center" alignItems="flex-start">
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  id="copyName"
+                  label="New document name"
+                  style={{ margin: 8 }}
+                  placeholder="New document name"
+                  fullWidth
+                  margin="normal"
+                  onChange={this.handleNewDocument}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+              {this.state.moustachesArray.map(item => {
+                return (
+                  <TextField
+                    required
+                    key={item}
+                    id= {item}
+                    label={item.toUpperCase()}
+                    style={{ margin: 8 }}
+                    placeholder={item}
+                    fullWidth
+                    margin="normal"
+                    onChange={handleInputs}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
+                );
+              })
+              }
+              </Grid>
+              <Grid item xs={12}>
+              {this.state.tripleMoustachesArray.map(item=>{
+                return (
+                  <TextField
+                    required
+                    id={item}
+                    key={item}
+                    name={item}
+                    label={item.toUpperCase()}
+                    type='file'
+                    onChange={handleChangeFile}
+                    fullWidth
+                    margin='normal'
+                    ref={fileInput}
+                  />
+                );
+              })
+              }
+              </Grid>
+              <Grid item xs={12} className={classes.itemBtn}>
+                <Link to="/steps/success" className={classes.links}>
+                  <Fab
+                    variant="extended"
+                    size="large"
+                    color="primary"
+                    aria-label="Add"
+                    className={classes.btnSend}
+                    onClick={this.loadSlidesReplace}>
+                    <i className={`fas fa-share-square ${classes.marginIcon}`}></i>
+                    <span>Send Form</span>
+                  </Fab>
+                </Link>
+              </Grid>
+            </Grid>
+          </Paper>
         </form>
       )
     }
@@ -121,34 +223,26 @@ class Fill extends Component {
   }
 
   render() {
-    const { selectedTemplate } = this.props;
+    const { selectedTemplate, classes } = this.props;
 
     if (this.state.moustachesArray.length>0 || this.state.tripleMoustachesArray.length>0 ) {
       return (
-        <div className="fill-page">
-          <div className="fill-template__result">
-            <div id="result">{selectedTemplate}</div>
-            <div className="fill-page__btn back-btn">
-              <button type="button" className="btn btn-light"><Link to="/steps/choose">Choose another template</Link></button>
-            </div>
-          </div>
-          <div className="input-name">
-                <label htmlFor="copyName">New document name: </label>
-                <input className="input-name" id= "copyName" type="text" onKeyUp={this.handleNewDocument} />
-              </div>
-          <div className="fill-page__form">
+        <div>
+          <Paper className={classes.message} elevation={1}>
+            <Grid container justify="center" alignItems="center" spacing={16}>
+              <Grid item>
+                <i class="far fa-check-circle fa-3x"></i>
+              </Grid>
+              <Grid item>
+              <Typography component="p" className={classes.paperFill}>
+                {selectedTemplate} <Button className={classes.btn} size="small"><Link className={classes.links} to="/steps/choose">Choose another template</Link></Button>
+              </Typography>
+              </Grid>
+            </Grid>
+          </Paper>
           {this.paintForm()}
-          </div>
-          <div className="row d-flex justify-content-around">
-            <div className="fill-page__btn back-btn">
-              <Link to="/steps/choose"><button type="button" className="btn btn-light">Back</button></Link>
-              <div className="fill-page__btn next-btn">
-                <Link to="/steps/success"><button type="button" className="btn btn-light" onClick={this.loadSlidesReplace}>Next</button></Link>
-              </div>
-            </div>
-          </div>
         </div>
-      );
+          );
     } else {
       return (
         <ReactLoading type={'spinningBubbles'} color={'#990099'} height={100} width={100} />
@@ -163,8 +257,8 @@ Fill.propTypes = {
   handleInputs: PropTypes.func,
   handleTripleMoustaches: PropTypes.func,
   inputs: PropTypes.array,
-  selectedTemplate: PropTypes.string
+  selectedTemplate: PropTypes.string,
+  classes: PropTypes.object.isRequired
 };
 
-export default Fill;
-
+export default withStyles(styles)(Fill);
