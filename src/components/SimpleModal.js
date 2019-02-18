cb071e61cfe218b222db73410d0d5d4014d37260
roxsb <rox.san.bri@gmail.com>
2019-02-18 10:59:14 +0100

    fixed position modal how to use

--- src/components/SimpleModal.js
+++ src/components/SimpleModal.js
@@ -10,8 +10,8 @@ function rand() {
 }
 
 function getModalStyle() {
-  const top = 50 + rand();
-  const left = 50 + rand();
+  const top = 50;
+  const left = 50;
 
   return {
     top: `${top}%`,
